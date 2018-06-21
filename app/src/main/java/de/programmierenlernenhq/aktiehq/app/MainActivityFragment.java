d13ee85404aa70022243ac63874537fef7cdeebd
fefe131 <38761388+fefe131@users.noreply.github.com>
2018-06-21 11:20:46 +0200

    new files

--- app/src/main/java/de/programmierenlernenhq/aktiehq/app/MainActivityFragment.java
+++ app/src/main/java/de/programmierenlernenhq/aktiehq/app/MainActivityFragment.java
@@ -5,6 +5,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivityFragment extends Fragment{
 
@@ -15,6 +21,27 @@ public class MainActivityFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        String [] aktienlisteArray = {
+                "Adidas - Kurs: 73,45 €",
+                "Allianz - Kurs: 145,12 €",
+                "BASF - Kurs: 84,27 €",
+                "Bayer - Kurs: 128,60 €",
+                "Beiersdorf - Kurs: 80,55 €",
+                "BMW St. - Kurs: 104,11 €",
+                "Commerzbank - Kurs: 12,47 €",
+                "Continental - Kurs: 209,94 €",
+                "Daimler - Kurs: 84,33 €"
+        };
+
+        List<String> aktienListe = new ArrayList<>(Arrays.asList(aktienlisteArray));
+
+        ArrayAdapter<String> aktienlisteAdapter = new ArrayAdapter<>(getActivity(), R.layout.list_item_aktienliste, R.id.list_item_aktienliste_textview, aktienListe);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView aktienlisteListView = (ListView) rootView.findViewById(R.id.listview_aktienliste);
+        aktienlisteListView.setAdapter(aktienlisteAdapter);
 
         return inflater.inflate(R.layout.fragment_main, container, false);
 
